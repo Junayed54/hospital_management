2ad18eb247f4bbd2b791f9f55e87c1822a2cb172
Junayed-shanto <junayedshanto54@gmail.com>
2024-12-05 00:13:55 +0600

    added whitenoise

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,9 +138,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'hospital/static'),  # Add the path to hospital static
+    os.path.join(BASE_DIR, 'static'),  # Add the path to hospital static
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
