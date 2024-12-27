8e660d6f4a7966d45d24e63021c0e12fcea6cbaa
Junayed-shanto <junayedshanto54@gmail.com>
2024-12-27 23:39:32 +0600

    add database in sittings.py

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -91,24 +91,24 @@ WSGI_APPLICATION = 'hospital_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'hospital_management',
-#         'USER': 'root',
-#         'PASSWORD': 'Bridgers@123',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hospital_management',
+        'USER': 'root',
+        'PASSWORD': 'Bridgers@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 
 
