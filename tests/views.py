b503a30e1702c754223485e89e83caca68d4d2e7
Junayed-shanto <junayedshanto54@gmail.com>
2025-03-04 15:02:57 +0600

    import generic in test/views

--- tests/views.py
+++ tests/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.utils.timezone import now
+from rest_framework import generics
+
 from django.contrib.auth import get_user_model
 User = get_user_model() 
 from accounts.permissions import *
